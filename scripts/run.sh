43983b1c8ea9422ef34c85d268d2bac162e80986
RusNor <85447162+RusNor@users.noreply.github.com>
2021-11-15 23:36:08 +0300

    Add files via upload

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,1 @@
-env "/home/rus_nor/StartWine/data/scripts/start" ""
+env "/home/rus_nor/StartWine/data/scripts/start" "/mnt/5f3d2f1f-b25e-489e-be2f-170f039408e5/Games/Dead Space/Dead Space.exe /mnt/5f3d2f1f-b25e-489e-be2f-170f039408e5/Games/Dead Space/Dead Space.exe /mnt/5f3d2f1f-b25e-489e-be2f-170f039408e5/Games/Dead Space/Dead Space.exe"
