b58d30342019d50249e718f1285d240d8fe32ba3
RusNor <85447162+RusNor@users.noreply.github.com>
2022-02-18 19:53:48 +0300

    Add files via upload

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,1 @@
-env "/home/rus_nor/StartWine/data/scripts/start" ""
+env "/home/rus_nor/StartWine/data/scripts/start" "/mnt/usb-WDC_WD50_00BEVT-24A0RT0_98765432100C-0:0-part4/Games/Mass Effect 2/Binaries/MassEffect2.exe"
